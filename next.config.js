721666413d52732df87359caa8952b6c4e2f0320
frammkor <frammkor@hotmail.com>
2023-09-25 01:32:00 -0300

    modified next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+    assetPrefix: isProd
+        ? '/your-github-repo-name/'
+        : '',
+    images: {
+        unoptimized: true
+    }
+}
