f22b64f859f1bab792aaaef7573f08b740b9da69
frammkor <frammkor@hotmail.com>
2023-10-12 16:48:27 -0300

    Deploy

---
+++ _next/static/R7GmE1vH0pODNey5W0Pas/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-96614af6579d1517.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
